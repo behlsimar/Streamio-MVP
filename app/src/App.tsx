744ed705fae4fb71410c3757dcf88286b27d823c
behlsimar <38884356+behlsimar@users.noreply.github.com>
2025-08-21 02:24:29 +1000

    Delete app/src directory

--- app/src/App.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import RootNav from './navigation/Tabs';
-export default function App(){ return <RootNav/> }
--- app/src/config.ts
+++
@@ -1,1 +0,0 @@
-export const BACKEND_URL = 'https://streamio-mvp.onrender.com'; // change to your Render URL
